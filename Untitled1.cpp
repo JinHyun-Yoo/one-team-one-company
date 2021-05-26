6655f4bc537857fb417468c248f628b68b9fc744
PC_07\user <rhj9542@naver.com>
2021-05-26 19:27:16 +0900

    사용자 이름 입력 구현

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,10 +1,16 @@
 #include<stdio.h>
 int main()
 {
+	char name[10];
 	int age;
+	
+	printf("당신의 이름을 입력하세요: ");
+	scanf("%s",&name);
 	
 	printf("당신의 나이를 입력하세요: ");
 	scanf("%d", &age);
 	
-	printf("우와 당신은 %d살이군요!", age); 
+	
+	
+	printf("반갑습니다. %s님!\n우와 당신은 %d살이군요!",name, age); 
 }
